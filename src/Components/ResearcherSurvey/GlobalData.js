82d7574b1c4d38feae35786cbf49d7cf3d0372e2
RozerinB <rozbaran@icloud.com>
2023-03-04 01:19:50 +0000

    reversed structure of global data

--- src/Components/ResearcherSurvey/GlobalData.js
+++ src/Components/ResearcherSurvey/GlobalData.js
@@ -10,7 +10,7 @@ import countryList from 'react-select-country-list';
 import { useField } from 'formik';
 import { competency, ethnicGroups, genders, languages, nationalities } from '../ParticipantSurvey/Questions';
 import DownloadIcon from '@mui/icons-material/Download';
-import { BarChart, DesignPrinciplesPieChart, DoughnutChart, HorizontalBarChart, ScatterChart } from '../GlobalData/Chart';
+import Chart from 'chart.js/dist/Chart.js'
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -381,6 +381,314 @@ const GlobalData = (props) => {
     };
   }
 
+   function ScatterChart({data, title, x, y, labels}) {
+    const canvasRef = useRef(null);
+    useEffect(() => {
+      const canvas = canvasRef.current;
+      if (canvas) {
+        const ctx = canvas.getContext('2d');
+        const scatterChart = new Chart(canvas, {
+          type: 'scatter',
+          data: {
+            datasets: [
+              {
+                label:title,
+                data: data,
+                backgroundColor: 'rgba(54, 162, 235, 1)'
+              }
+            ]
+          },
+          options: {
+            title: {
+              display: true,
+              text: title,
+              fontSize: 18
+            },
+            scales: {
+              xAxes: [{ 
+                position: 'bottom',
+                scaleLabel: {
+                  display: true,
+                  labelString:  x
+                }
+              }],
+              yAxes: [{
+                type: 'category',
+                labels: labels,
+                ticks: {
+                  reverse: true
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: y
+                }
+              }]
+            }
+          }
+        });
+      }
+    }, []);
+    return <canvas style = {{position: 'relative', width:"300" , height:"300"}}ref={canvasRef} />;
+  }
+
+ function BarChart({data, title, x , y}) {
+    const canvasRef = useRef(null);
+    useEffect(() => {
+      const canvas = canvasRef.current;
+      if (canvas) {
+        const ctx = canvas.getContext('2d');
+        const chart = new Chart(canvas, {
+          type: 'bar',
+          data: data,
+          options: {
+            plugins: {
+              colors: {
+                forceOverride: true
+              }
+            },
+            title: {
+              display: true,
+              text: title,
+              fontSize: 18
+            },
+            legend: {
+              display: true,
+              position: 'bottom',
+              labels: {
+                fontColor: '#333',
+                fontSize: 14
+              }
+            },
+            scales: {
+              xAxes: [{
+                ticks: {
+                  fontColor: '#333',
+                  fontSize: 14,
+                  beginAtZero: true
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: x,
+                  fontColor: '#333',
+                  fontSize: 16
+                }
+              }],
+              yAxes: [{
+                stacked: false,
+                ticks: {
+                  fontColor: '#333',
+                  fontSize: 14,
+                  beginAtZero: true
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: y,
+                  fontColor: '#333',
+                  fontSize: 16
+                }
+              }]
+            }
+          }
+        });
+      }
+    }, []);
+    return <canvas style = {{position: 'relative' , width:"300" , height:"300"}}ref={canvasRef} />;
+  }
+
+ function DesignPrinciplesPieChart({ data, title }){
+    const [chartData, setChartData] = useState({});
+
+    useEffect(() => {
+      const count1s = () => {
+        const counts = {};
+        data.forEach((item) => {
+          Object.keys(item.design_principles).forEach((key) => {
+            if (item.design_principles[key] === 1) {
+              if (counts[key]) {
+                counts[key] += 1;
+              } else {
+                counts[key] = 1;
+              }
+            }
+          });
+        });
+        return counts;
+      };
+
+      const chart = () => {
+        const counts = count1s();
+        setChartData({
+          labels: [
+            'Principle 1',
+            'Principle 2',
+            'Principle 3',
+            'Principle 4',
+            'Principle 5',
+            'Principle 6',
+            'Principle 7',
+          ],
+          datasets: [
+            {
+              label: 'Design Principles',
+              data: [
+                counts.principle1 || 0,
+                counts.principle2 || 0,
+                counts.principle3 || 0,
+                counts.principle4 || 0,
+                counts.principle5 || 0,
+                counts.principle6 || 0,
+                counts.principle7 || 0,
+              ],
+              backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(189, 195, 199, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 148, 112, 0.2)',
+              ],
+            },
+          ],
+        });
+      };
+      chart();
+    }, [data]);
+  
+    const canvasRef = useRef(null);
+  
+    useEffect(() => {
+      const ctx = canvasRef.current.getContext('2d');
+      const chart = new Chart(ctx, {
+        type: 'pie',
+        data: chartData,
+        options: {
+          plugins: {
+            colors: {
+              forceOverride: true
+            }
+          },
+          title: {
+            display: true,
+            text: title,
+            fontSize: 18
+          },
+        
+          }
+      });
+    }, [chartData]);
+  
+    return <canvas style={{ position: 'relative',  width:"200" , height:"200" }} ref={canvasRef} />;
+  };
+
+
+ function DoughnutChart({ data, title }) {
+    const canvasRef = useRef(null);
+  
+    useEffect(() => {
+      const ctx = canvasRef.current.getContext('2d');
+      const chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [{
+            data: [data.elementYesCount || 0 , data.elementNoCount || 0],
+            backgroundColor: [
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+            ],
+          }],
+          labels: ['Yes', 'No'],
+        },
+        options: {
+          plugins: {
+            colors: {
+              forceOverride: true,
+            },
+          },
+          title: {
+            display: true,
+            text: title,
+            fontSize: 18,
+          },
+        },
+      });
+    }, []);
+  
+    return <canvas style={{ position: 'relative', width: "300", height: "300" }} ref={canvasRef} />;
+  }
+  
+ function HorizontalBarChart({data, element, title, countryArray}) {
+    // Extract language and country data from input
+    const languagesInElement = data.map(d => d[element]);
+    const countries = data.map(d => d.country);
+
+    // Get unique language and country values
+    const uniqueLanguages = [...new Set(languagesInElement)];
+    const uniqueCountries = [...new Set(countries)];
+
+    // Initialize data object with zeros for each language
+    const dataObject = {};
+    uniqueLanguages.forEach(lang => {
+        dataObject[lang] = new Array(uniqueCountries.length).fill(0);
+    });
+
+    const countryLabelsMap = uniqueCountries.map(c => c);
+    const chartCountryLabel = countryArray.filter(country => countryLabelsMap.includes(country.value)).map(country => country.label);
+
+    // Fill in data for each language and country
+    data.forEach(d => {
+        const langIndex = uniqueLanguages.indexOf(d[element]);
+        const countryIndex = uniqueCountries.indexOf(d.country);
+        dataObject[d[element]][countryIndex]++;
+    });
+
+    // Generate datasets array dynamically
+    const baseColor = "rgba(54, 162, 235, 1)";
+    const colorCount = Object.keys(dataObject).length;
+    const datasets = Object.keys(dataObject).map((lang, index) => {
+    // Find the language label from the languages array
+    const langObject = languages.find(l => l.value === lang);
+    const langLabel = langObject ? langObject.label : lang;
+    const color = Chart.helpers.color(baseColor).lighten(index / colorCount).rgbString();
+    return {
+        label: langLabel,
+        data: dataObject[lang],
+        backgroundColor: color,
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1
+    }
+});
+
+    // Initialize chart
+    const canvasRef = useRef(null);
+  
+    useEffect(() => {
+      const ctx = canvasRef.current.getContext('2d');
+      const chart = new Chart(ctx, {
+        type: "horizontalBar",
+        data: {
+            labels: chartCountryLabel,
+            datasets: datasets
+        },
+        options: {
+          scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        title: {
+          display: true,
+          text: title,
+          fontSize: 18,
+        },
+        }
+    });
+  }, []);
+    return <canvas style={{ position: 'relative', width: "300", height: "300" }} ref={canvasRef} />;
+  }
+
   return (
     <div className='survey-heading'>  
     <Typography variant="h6">
@@ -447,8 +755,8 @@ const GlobalData = (props) => {
         <BarChart data={createTechnologyChartData(nationalitiesInSelectedCountryLabel, technologyDeviceAccessByNationalityInSelectedCountry)} title = {"Device Type Participant has access to by Nationality In Selected Country"}  x ={"Device Type"} y ={"Number of Participants"}/>  
         <BarChart data={createTechnologyChartData(genderInSelectedCountryLabel, technologyDeviceAccessByGenderInSelectedCountry)} title = {"Device Type Participant has access to by Gender In Selected Country"}  x ={"Device Type"} y ={"Number of Participants"}/>  
         
-        <HorizontalBarChart data={dataForCountrySelected} element={'preferred_reading_language'} title={'Preferred Reading Language By Selected Country'} countryArray={countryArray}/>
-        <HorizontalBarChart data={dataForCountrySelected} element={'preferred_writing_language'} title={'Preferred Writing Language By Selected Country'} countryArray={countryArray}/>
+        <HorizontalBarChart data={dataForCountrySelected} element={'preferred_reading_language'} title={'Preferred Reading Language By Selected Country'} />
+        <HorizontalBarChart data={dataForCountrySelected} element={'preferred_writing_language'} title={'Preferred Writing Language By Selected Country'} />
         <BarChart data={createTextDirectionalityChartData(selectedCountryLabel, textDirectionalityBySelectedCountry)} title = {"Text Directionality by Selected Country"}  x ={"Text Directionality"} y ={"Number of Participants"}/>  
         <BarChart data={createTextDirectionalityChartData(chartEthnicityInSelectedCountryLabel, textDirectionalityByEthnicityInSelectedCountry)} title = {"Text Directionality by Ethnicity In Selected Country"}  x ={"Text Directionality"} y ={"Number of Participants"}/> 
         <BarChart data={createTextDirectionalityChartData(nationalitiesInSelectedCountryLabel, textDirectionalityByNationalityInSelectedCountry)} title = {"Text Directionality by Nationality In Selected Country"}  x ={"Text Directionality"} y ={"Number of Participants"}/> 
@@ -478,8 +786,8 @@ const GlobalData = (props) => {
         </Typography>
         </Grid>
       </Grid>
-      <HorizontalBarChart data={data} element={'preferred_reading_language'} title={'Preferred Reading Language By Country'} countryArray={countryArray}/>
-      <HorizontalBarChart data={data} element={'preferred_writing_language'} title={'Preferred Writing Language By Country'} countryArray={countryArray}/>
+      <HorizontalBarChart data={data} element={'preferred_reading_language'} title={'Preferred Reading Language By Country'} />
+      <HorizontalBarChart data={data} element={'preferred_writing_language'} title={'Preferred Writing Language By Country'} />
       <BarChart data={createTextDirectionalityChartData(chartCountryLabel, textDirectionalityByCountry)} title = {"Text Directionality by Country"}  x ={"Text Directionality"} y ={"Number of Participants"}/>  
       <BarChart data={createTextDirectionalityChartData(chartEthnicityLabel, textDirectionalityByEthnicity)} title = {"Text Directionality by Ethnicity"}  x ={"Text Directionality"} y ={"Number of Participants"}/> 
       <BarChart data={createTextDirectionalityChartData(chartNationalityLabel, textDirectionalityByNationality)} title = {"Text Directionality by Nationality"}  x ={"Text Directionality"} y ={"Number of Participants"}/> 

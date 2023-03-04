2c7cc96efc1e5225733f54241171d7d86e43c1e2
RozerinB <rozbaran@icloud.com>
2023-03-04 18:29:16 +0000

    added new fields to evaluation survey

--- src/Components/ResearcherSurvey/ResearcherDataCollectionSurvey.jsx
+++ src/Components/ResearcherSurvey/ResearcherDataCollectionSurvey.jsx
@@ -4,45 +4,67 @@ import { Box, Grid } from '@mui/material';
 import RadioField from '../FormFields/RadioField.jsx';
 import { scale, yesOrNo } from '../ParticipantSurvey/Questions.js';
 import InputField from '../FormFields/InputField.jsx';
-import SelectField from '../FormFields/SelectField.jsx';
 
 export default function ResearcherDataCollectionSurvey (props) {
   const {
     formField: {
       usefulness_for_profession,
+      usefulness_for_profession_reason,
       usefulness_for_researcher,
+      usefulness_for_researcher_reason,
       design_globally,
+      design_globally_reason,
       design_for_non_western,
+      design_for_non_western_reason,
       design_for_western,
+      design_for_western_reason,
       feedback,
     }
   } = props;
 
   return (
     <div className='survey-heading'> 
-        <Typography variant="h6">
+      <Typography variant="h6">
         <Box sx={{ fontWeight: 'bold', m: 1, p:2 }}> Evaluation Survey </Box>
       </Typography>
+      <Typography variant="caption">
+        <Box sx={{ pl:5 }}> *Please note that if you close the tab, all the data you have inputted will be lost </Box>
+      </Typography>
       <div className='survey-questions'> 
-      <Typography variant="h6">
+      <Typography variant="h6" sx={{mb: 1}}>
         <Box sx={{ fontWeight: 'bold'}}> Usability Engineering and the importance of the level of exposure to technology </Box>
       </Typography>
       <Grid container spacing={3}> 
         <Grid item xs={12} >
-          <RadioField name={usefulness_for_profession.name} label={usefulness_for_profession.label} data = {scale} row={true} fullWidth />
+          <RadioField name={usefulness_for_profession.name} label={usefulness_for_profession.label} data={scale} row={true} fullWidth />
         </Grid>   
         <Grid item xs={12} >
-          <SelectField name={usefulness_for_researcher.name} label={usefulness_for_researcher.label} data = {yesOrNo} fullWidth />
+        <InputField name={usefulness_for_profession_reason.name} label={usefulness_for_profession_reason.label} placeholder={usefulness_for_profession_reason.placeholder} fullWidth />
         </Grid>
         <Grid item xs={12} >
-          <SelectField name={design_globally.name} label={design_globally.label} data={yesOrNo} fullWidth />
+          <RadioField name={usefulness_for_researcher.name} label={usefulness_for_researcher.label} data = {yesOrNo} fullWidth />
+        </Grid>
+        <Grid item xs={12} >
+        <InputField name={usefulness_for_researcher_reason.name} label={usefulness_for_researcher_reason.label} placeholder={usefulness_for_researcher_reason.placeholder} fullWidth />
+        </Grid>
+        <Grid item xs={12} >
+          <RadioField name={design_globally.name} label={design_globally.label} data={yesOrNo} fullWidth />
         </Grid>    
         <Grid item xs={12} >
-          <SelectField name={design_for_non_western.name} label={design_for_non_western.label} data = {yesOrNo} fullWidth />
+        <InputField name={design_globally_reason.name} label={design_globally_reason.label} placeholder={design_globally_reason.placeholder} fullWidth />
+        </Grid>
+        <Grid item xs={12} >
+          <RadioField name={design_for_non_western.name} label={design_for_non_western.label} data = {yesOrNo} fullWidth />
+        </Grid>  
+        <Grid item xs={12} >
+        <InputField name={design_for_non_western_reason.name} label={design_for_non_western_reason.label} placeholder={design_for_non_western_reason.placeholder} fullWidth />
+        </Grid> 
+        <Grid item xs={12} >
+          <RadioField name={design_for_western.name} label={design_for_western.label} data = {yesOrNo} fullWidth />
         </Grid>   
         <Grid item xs={12} >
-          <SelectField name={design_for_western.name} label={design_for_western.label} data = {yesOrNo} fullWidth />
-        </Grid>   
+        <InputField name={design_for_western_reason.name} label={design_for_western_reason.label} placeholder={design_for_western_reason.placeholder} fullWidth />
+        </Grid>
         <Grid item xs={12} >
           <InputField name={feedback.name} label={feedback.label} fullWidth />
         </Grid>                
--- src/Components/ResearcherSurvey/SurveyInitialValues.js
+++ src/Components/ResearcherSurvey/SurveyInitialValues.js
@@ -5,12 +5,16 @@ const {
     consent_form,
     client_id,
     usefulness_for_profession,
+    usefulness_for_profession_reason,
     usefulness_for_researcher,
+    usefulness_for_researcher_reason,
     design_globally,
+    design_globally_reason,
     design_for_non_western,
+    design_for_non_western_reason,
     design_for_western,
+    design_for_western_reason,
     feedback,
-    country
   }
 } = surveyFormModel;
 
@@ -19,10 +23,14 @@ export default {
   [consent_form.name]: false,
   [client_id.name]: '',
   [usefulness_for_profession.name]:'',
+  [usefulness_for_profession_reason.name]:'',
   [usefulness_for_researcher.name]:'',
+  [usefulness_for_researcher_reason.name]:'',
   [design_globally.name]: '',
+  [design_globally_reason.name]: '',
   [design_for_non_western.name]: '',
+  [design_for_non_western_reason.name]: '',
   [design_for_western.name]: '',
+  [design_for_western_reason.name]: '',
   [feedback.name]: '',
-  [country.name]: ''
 };
